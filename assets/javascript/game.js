f92575cfd891ad0c1e65fea4152cd38daebb5ddb
ralexander <theravenalexander@gmail.com>
2018-08-14 17:29:26 -0600

    comments and page tweaks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,3 +26,111 @@
 
 // The random number shown at the start of the game should be between 19 - 120.
 // Each crystal should have a random hidden value between 1 - 12.
+
+//GLOBAL VARIABLES
+
+var crystal = {
+    blue: {
+        name: "Blue",
+        value: 0
+    },
+    green: {
+        name: "Green",
+        value: 0
+    },
+    pink: {
+        name: "Pink",
+        value: 0
+    },
+    yellow: {
+        name: "Yellow",
+        value: 0
+    }
+};
+
+var currentScore = 0;
+var targetScore = 0;
+
+var winCount = 0;
+var lossCount = 0;
+
+//FUNCTIONS
+var getRandom = function(min, max) {
+    return Math.floor(Math.random() * max - min + 1) + min;
+}
+
+var startGame = function() {
+    //Reset the current score
+    currentScore = 0;
+    //Set a new target score between 19 and 120
+    targetScore = getRandom(19, 120);
+    //Set different values for each of the crystals between 1 and 12
+    crystal.blue.value = getRandom(1, 12);
+    crystal.pink.value = getRandom(1, 12);
+    crystal.green.value = getRandom(1, 12);
+    crystal.yellow.value = getRandom(1, 12);
+
+    //Change the HTML to reflect all of these changes
+    $("#yourScore").html("Current Score: " + currentScore);
+    $("#targetScore").html("Target Score: " + targetScore);
+    
+}
+
+var resetGame = function() {
+    //Reset the current score
+    currentScore = 0;
+    //Set a new target score between 19 and 120
+    targetScore = getRandom(19, 120);
+    //Set different values for each of the crystals between 1 and 12
+    crystal.blue.value = getRandom(1, 12);
+    crystal.pink.value = getRandom(1, 12);
+    crystal.green.value = getRandom(1, 12);
+    crystal.yellow.value = getRandom(1, 12);
+};
+
+// Respond to click on the crystals
+var addValues = function(crystal) {
+//change currentscore
+currentScore = currentScore + crystal.value;
+
+//Change the HTML to reflect changes on currentScore
+$("#yourScore").html(currentScore);
+
+};
+
+/* var gamePlay = function() {
+    if(currentScore > targetScore) {
+        lossCount++;
+        $("#lossCount").html(lossCount);
+        resetGame();
+        //I am not sure why the game will not reset ?
+
+    } 
+    if(currentScore === targetScore) {
+
+        winCount++;
+        $("#winCount").html(winCount);
+        resetGame();
+
+    };
+    }; */
+
+//MAIN PROCESS
+//Starts the game the first time
+startGame();
+gamePlay();
+$('#blue').click(function () {
+    addValues(crystal.blue);
+});
+
+$('#pink').click(function () {
+    addValues(crystal.pink);
+});
+
+$('#green').click(function () {
+    addValues(crystal.green);
+});
+
+$('#yellow').click(function () {
+    addValues(crystal.yellow);
+});
